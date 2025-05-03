1b4cafd60f582e0e39b22b1730ad1d5cc9a15f34
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2025-05-03 17:14:05 +0300

    feat: Implement lemonade app with interactive steps
    This commit implements the core functionality of the lemonade app.
    - It replaces the initial placeholder with a `LemonadeApp` composable function.
    - It introduces `currentStep` and `squeezeCount` state variables to manage the app's state.
    - It includes a `LemonWithTextAndImage` composable function to display images and text, updating based on the current step.
    - It uses click interactions to change steps, including a random squeeze count for step 2.
    - It adds resources: image drawables (`lemon_tree`, `lemon_squeeze`, `lemon_drink`, `lemon_restart`) and strings for different steps.

--- app/src/main/java/com/aliozdemir/lemonade/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/lemonade/MainActivity.kt
@@ -3,45 +3,104 @@ package com.aliozdemir.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aliozdemir.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LemonadeApp()
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun LemonadeApp() {
+    LemonWithTextAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LemonadeTheme {
-        Greeting("Android")
+fun LemonWithTextAndImage(modifier: Modifier = Modifier) {
+    var currentStep by remember { mutableIntStateOf(1) }
+    var squeezeCount by remember { mutableIntStateOf(0) }
+
+    val lemonImage = when (currentStep) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    val lemonText = when (currentStep) {
+        1 -> R.string.lemon_select
+        2 -> R.string.lemon_squeeze
+        3 -> R.string.lemon_drink
+        else -> R.string.lemon_empty_glass
+    }
+
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(lemonImage),
+            contentDescription = stringResource(lemonText),
+            modifier = Modifier
+                .background(
+                    shape = RoundedCornerShape(40.dp),
+                    color = MaterialTheme.colorScheme.tertiaryContainer
+                )
+                .clickable {
+                    currentStep = when (currentStep) {
+                        1 -> {
+                            squeezeCount = (2..4).random()
+                            2
+                        }
+
+                        2 -> {
+                            squeezeCount--
+                            if (squeezeCount == 0) 3 else 2
+                        }
+
+                        3 -> 4
+                        else -> 1
+                    }
+                }
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text(
+            text = stringResource(lemonText),
+            fontSize = 18.sp,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
